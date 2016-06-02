9b7b8d033495762a86e30074b85495a3cb7baefb
Dmitry Doroshev <r.schweppes@ya.ru>
2016-06-02 11:32:18 +0300

    check existing function

--- src/array_select.php
+++ src/array_select.php
@@ -1,19 +1,21 @@
 <?php
 
-function array_select($key, $array)
-{
-    if(!is_array($key)) {
-        return array_map(function($el) use ($key) { return $el[$key]; }, $array);
-    } else {
-        $keys = $key;
-        $result = array();
-        foreach ($array as $elKey => $el) {
-            $resultEl = array();
-            foreach ($keys as $key) {
-                $resultEl[$key] = $el[$key];
+if(!function_exists('array_select')) {
+    function array_select($key, $array)
+    {
+        if(!is_array($key)) {
+            return array_map(function($el) use ($key) { return $el[$key]; }, $array);
+        } else {
+            $keys = $key;
+            $result = array();
+            foreach ($array as $elKey => $el) {
+                $resultEl = array();
+                foreach ($keys as $key) {
+                    $resultEl[$key] = $el[$key];
+                }
+                $result[$elKey] = $resultEl;
             }
-            $result[$elKey] = $resultEl;
+            return $result;
         }
-        return $result;
     }
 }
