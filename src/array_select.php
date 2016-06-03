0f2787e3128f6a0a4e2bd9f92fa7682ff244b01c
Dmitry Doroshev <r.schweppes@ya.ru>
2016-06-04 01:09:30 +0300

    $array → $from

--- src/array_select.php
+++ src/array_select.php
@@ -5,24 +5,24 @@ if(!function_exists('array_select')) {
      * Извлечение значений по ключам из массива ассоциативных массивов
      *
      * @param $key string|array Ключ или массив ключей массива
-     * @param $array array Массив
+     * @param $from array Массив
      * @return array
      * @throws Exception
      */
-    function array_select($key, $array)
+    function array_select($key, $from)
     {
-        if(!is_array($array)) {
-            throw new Exception('Invalid $array type');
+        if(!is_array($from)) {
+            throw new Exception('Invalid $from type');
         }
 
         $result = array();
         if (!is_array($key)) {
-            foreach ($array as $el) {
+            foreach ($from as $el) {
                 $result[] = isset($el[$key]) ? $el[$key] : null;
             }
         } else {
             $keys = $key;
-            foreach ($array as $elKey => $el) {
+            foreach ($from as $elKey => $el) {
                 $resultEl = array();
                 foreach ($keys as $key) {
                     $resultEl[$key] = isset($el[$key]) ? $el[$key] : null;
